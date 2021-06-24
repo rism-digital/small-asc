873ea2f4e7de41f36e50eb5ac8e132f670c72201
Andrew Hankinson <andrew.hankinson@rism.digital>
2021-06-24 14:12:57 +0200

    Asynchronous was too hard
    Change the library to be synchronous for now. We can revisit async stuff later.
    
    Also, added a first pass at iterating all results.

--- README.txt
+++ README.txt
@@ -1,5 +1,5 @@
 # SMALL-ASC
 
-A small asynchronous Solr client, using HTTPX and UJSON. Built as an eventual replacement for PySolr in our projects.
+A small Solr client, using HTTPX and UJSON. Built as an eventual replacement for PySolr in our projects.
 
 Currently experimental.
--- small_asc/client.py
+++ small_asc/client.py
@@ -16,8 +16,8 @@ class JsonAPIRequest(TypedDict, total=False):
     query: str
     filter: Union[str, list[str]]
     params: dict
-    offset: int
-    limit: int
+    offset: int  # aka 'start'
+    limit: int   # aka 'rows'
     sort: str
     fields: list[str]
     facet: list[dict]
@@ -25,31 +25,73 @@ class JsonAPIRequest(TypedDict, total=False):
 
 class Results:
     """
-    Mimics the pysolr Request object to make migration easier.
-    """
-    def __init__(self, result_json: dict, next_page_query=None):
-        self.raw_response = result_json
+    Originally based on the pysolr Request object, but with some changes in behaviour to support more natural
+    cursor iteration.
 
-        response_part: dict = result_json.get("response", {})
+    """
+    def __init__(self, result_json: dict,
+                 url: Optional[str] = None,
+                 query: Optional[JsonAPIRequest] = None,
+                 session: Optional[httpx.Client] = None):
+
+        self.raw_response: dict = result_json
+        self.__set_instance_values(result_json)
+
+        # parameters supporting cursor-based results
+        self._query_url: Optional[str] = url
+        self._query: Optional[JsonAPIRequest] = query
+        self._session: Optional[httpx.Client] = session
+
+        # condense the check into a single boolean
+        self._is_cursor: bool = all((self._query_url, self._query, self._session))
+        self._idx: int = 0
+        self._page_idx: int = 0
+
+    def __set_instance_values(self, raw_response: dict) -> None:
+        response_part: dict = raw_response.get("response", {})
         self.docs: list = response_part.get("docs", [])
         self.hits: int = response_part.get("numFound", 0)
 
         # other response metadata
-        self.debug = result_json.get("debug", {})
-        self.highlighting = result_json.get("highlighting", {})
-        self.facets = result_json.get("facet_counts", {})
-        self.spellcheck = result_json.get("spellcheck", {})
-        self.stats = result_json.get("stats", {})
-        self.qtime = result_json.get("responseHeader", {}).get("QTime", None)
-        self.grouped = result_json.get("grouped", {})
-        self.nextCursorMark = result_json.get("nextCursorMark", None)
-        self._next_page_query = self.nextCursorMark is not None and next_page_query or None
+        self.debug = raw_response.get("debug", {})
+        self.highlighting = raw_response.get("highlighting", {})
+        self.facets = raw_response.get("facet_counts", {})
+        self.spellcheck = raw_response.get("spellcheck", {})
+        self.stats = raw_response.get("stats", {})
+        self.qtime = raw_response.get("responseHeader", {}).get("QTime", None)
+        self.grouped = raw_response.get("grouped", {})
+
+        self.nextCursorMark = raw_response.get("nextCursorMark", None)
 
     def __len__(self):
-        if self._next_page_query:
+        if self._is_cursor:
             return self.hits
         else:
             return len(self.docs)
+
+    def __iter__(self):
+        if self._is_cursor is False:
+            yield from self.docs
+        else:
+            while self._idx < self.hits:
+                try:
+                    yield self.docs[self._page_idx]  # type: ignore
+                except IndexError:
+                    self._page_idx = 0
+                    # update the cursormark with the cursor mark from the previous query.
+                    self._query.get("params", {}).update({
+                        "cursorMark": self.nextCursorMark
+                    })
+                    self.raw_response = _post_data_to_solr(self._query_url, self._query, self._session)
+                    self.__set_instance_values(self.raw_response)
+
+                    if self.docs:
+                        yield self.docs[self._page_idx]
+                    else:
+                        break
+
+                self._page_idx += 1
+                self._idx += 1
 
 
 class Solr:
@@ -66,27 +108,57 @@ class Solr:
             self,
             url: str
     ):
-        self._session = httpx.AsyncClient()
+        self._session = httpx.Client()
         self._url: str = url
 
-    async def search(self, query: JsonAPIRequest, handler: str = "/select") -> Results:
+    def search(self, query: JsonAPIRequest, cursor: bool = False, handler: str = "/select") -> Results:
         """
         Consumes a Solr JSON Request API configuration.
 
+        The 'cursor' parameter, False by default, determines the behaviour of the Results. If it is false,
+        iterating over the results will return only the list of rows returned in a paginated query, and retrieving
+        subsequent pages must be done with the 'offset' (start) and 'limit' (rows) parameters.
+
+        If 'cursor' is true, then iterating over the results will cause the results class to automatically retrieve
+        the next page, so that the full list of results can be returned simply by doing 'for doc in results'....
+
         :param query: A dictionary corresponding to a Solr JSON Request API configuration for a query
+        :param cursor: A boolean that determines whether a cursor is used in the search results.
         :param handler: A Solr handler endpoint to target the query
         :return:
         """
         url: str = self._create_url(handler)
-        search_results: dict = await self._send_to_solr(url, query)
+
+        if cursor:
+            if 'offset' in query or 'start' in query.get("params", {}):
+                raise SolrError("Offset or start is not supported when performing a cursor query.")
+
+            # 'legacy' solr query parameters can be stored in the 'params' key. Ensure we have one
+            # if it's not already passed in.
+            if 'params' not in query:
+                query['params'] = {}
+
+            query['params'].update({
+                "cursorMark": "*"
+            })
+
+            # cursor queries need to be explicitly sorted, which makes them not very useful
+            # for doing relevance search, but
+            if 'sort' not in query or 'sort' not in query.get("params", {}):
+                query.update({"sort": "id asc"})
+
+        search_results: dict = _post_data_to_solr(url, query, self._session)
+
+        if cursor:
+            return Results(search_results, url, query, self._session)
 
         return Results(search_results)
 
-    async def add(self, docs: list[dict], handler: str = "/update") -> dict:
+    def add(self, docs: list[dict], handler: str = "/update") -> dict:
         url: str = self._create_url(handler)
-        return await self._send_to_solr(url, docs)
+        return _post_data_to_solr(url, docs, self._session)
 
-    async def get(self, docid: str, handler: str = "/get") -> Optional[dict]:
+    def get(self, docid: str, handler: str = "/get") -> Optional[dict]:
         """
         Sends a request to the Solr RealtimeGetHandler endpoint to fetch a single
          record by its ID. Special consideration must be made to package up the
@@ -97,106 +169,44 @@ class Solr:
         :return: A dictionary containing the Solr document.
         """
         url: str = self._create_url(handler)
-        doc: dict = await self._send_to_solr(url, {"params": {"id": docid}})
+        doc: dict = _post_data_to_solr(url, {"params": {"id": docid}}, self._session)
 
         return doc.get("doc") or None
 
-    async def delete(self, query: str, handler: str = "/update") -> Optional[dict]:
+    def delete(self, query: str, handler: str = "/update") -> Optional[dict]:
         base_url: str = self._create_url(handler)
         # automatically commit the result of the delete query so we don't have
         # old docs hanging around.
         delete_url: str = f"{base_url}?commit=true"
-        res: dict = await self._send_to_solr(delete_url, {"delete": {"query": query}})
+        res: dict = _post_data_to_solr(delete_url, {"delete": {"query": query}}, self._session)
 
         return res
 
     def _create_url(self, handler: str) -> str:
         return "/".join([self._url.rstrip("/"), handler.lstrip("/")])
 
-    async def _send_to_solr(self, url: str, data: Union[list, dict]):
-        async with self._session as client:
-            try:
-                res = await client.post(url,
-                                        headers={'Content-Type': 'application/json'},
-                                        data=ujson.dumps(data))
 
-            except httpx.TimeoutException as err:
-                error_message: str = "Connection to server %s timed out: %s"
-                raise SolrError(error_message % (url, err))
-            except httpx.ConnectError as err:
-                error_message: str = "Failed to connect to server at %s: %s"
-                raise SolrError(error_message % (url, err))
-            except httpx.HTTPError as err:
-                error_message: str = "Unhandled connection error for %s: %s"
-                raise SolrError(error_message % (url, err))
+def _post_data_to_solr(url: str, data: Union[list, dict], connection: httpx.Client) -> dict:
+    with connection as client:
+        try:
+            res = client.post(url,
+                              headers={'Content-Type': 'application/json'},
+                              data=ujson.dumps(data))
 
-            if res.status_code != 200:
-                error_message: str = "Solr responded with HTTP Error %s: %s"
-                raise SolrError(error_message % (res.status_code, res.reason_phrase))
+        except httpx.TimeoutException as err:
+            error_message: str = "Connection to server %s timed out: %s"
+            raise SolrError(error_message % (url, err))
+        except httpx.ConnectError as err:
+            error_message: str = "Failed to connect to server at %s: %s"
+            raise SolrError(error_message % (url, err))
+        except httpx.HTTPError as err:
+            error_message: str = "Unhandled connection error for %s: %s"
+            raise SolrError(error_message % (url, err))
 
-            json_result = ujson.loads(res.text)
+        if res.status_code != 200:
+            error_message: str = "Solr responded with HTTP Error %s: %s"
+            raise SolrError(error_message % (res.status_code, res.reason_phrase))
 
-        return json_result
+        json_result = ujson.loads(res.text)
 
-
-class SyncSolr:
-    """
-    A synchronous version of the async connection. Used for places
-    where the async version might be problematic.
-    """
-    def __init__(self, url: str) -> None:
-        self._session = httpx.Client()
-        self._url: str = url
-
-    def search(self, query: JsonAPIRequest, handler: str = "/select") -> Results:
-        url: str = self._create_url(handler)
-        search_results: dict = self._send_to_solr(url, query)
-
-        return Results(search_results)
-
-    def add(self, docs: list[dict], handler: str = "/update"):
-        url: str = self._create_url(handler)
-        return self._send_to_solr(url, docs)
-
-    def get(self, docid: str, handler: str = "/get") -> Optional[dict]:
-        url: str = self._create_url(handler)
-        doc: dict = self._send_to_solr(url, {"params": {"id": docid}})
-
-        return doc.get("doc") or None
-
-    def delete(self, query: str, handler: str = "/update") -> dict:
-        base_url: str = self._create_url(handler)
-        # automatically commit the result of the delete query so we don't have
-        # old docs hanging around.
-        delete_url: str = f"{base_url}?commit=true"
-        res: dict = self._send_to_solr(delete_url, {"delete": {"query": query}})
-
-        return res
-
-    def _create_url(self, handler: str) -> str:
-        return "/".join([self._url.rstrip("/"), handler.lstrip("/")])
-
-    def _send_to_solr(self, url: str, data: Union[list, dict]):
-        with self._session as client:
-            try:
-                res = client.post(url,
-                                  headers={'Content-Type': 'application/json'},
-                                  data=ujson.dumps(data))
-
-            except httpx.TimeoutException as err:
-                error_message: str = "Connection to server %s timed out: %s"
-                raise SolrError(error_message % (url, err))
-            except httpx.ConnectError as err:
-                error_message: str = "Failed to connect to server at %s: %s"
-                raise SolrError(error_message % (url, err))
-            except httpx.HTTPError as err:
-                error_message: str = "Unhandled connection error for %s: %s"
-                raise SolrError(error_message % (url, err))
-
-            if res.status_code != 200:
-                error_message: str = "Solr responded with HTTP Error %s: %s"
-                raise SolrError(error_message % (res.status_code, res.reason_phrase))
-
-            json_result = ujson.loads(res.text)
-
-        return json_result
+    return json_result
