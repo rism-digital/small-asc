28ee5b8aa3f089a770382ab2fe9f78796b884a21
Andrew Hankinson <andrew.hankinson@gmail.com>
2022-06-07 09:23:02 +0200

    New: 0.9.8
    Update dependencies, etc.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='small-asc',
-    version='0.9.7',
+    version='0.9.8',
     packages=['small_asc'],
     url='https://github.com/rism-digital/small-asc',
     license='MIT',
