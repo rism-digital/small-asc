c822c78337702f1a1b169b92c8a0b76f12df2a68
Andrew Hankinson <andrew.hankinson@gmail.com>
2023-01-13 12:32:22 +0100

    v0.10.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='small-asc',
-    version='0.10.1',
+    version='0.10.2',
     packages=['small_asc'],
     package_data = {
         'small-asc': ['py.typed'],
