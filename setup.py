5b15eb009c7774afc18f51ddedd23c0eb5a24a30
Andrew Hankinson <andrew.hankinson@gmail.com>
2024-09-03 13:23:15 +0200

    Update dependencies

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="small-asc",
-    version="0.12.12",
+    version="0.12.13",
     packages=["small_asc"],
     package_data={
         "small-asc": ["py.typed"],
