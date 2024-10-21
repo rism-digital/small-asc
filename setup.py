03c3d4c4945bd46b6a1b4ffd8c3e82bc74aefb7d
Andrew Hankinson <andrew.hankinson@gmail.com>
2024-10-21 15:51:37 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="small-asc",
-    version="0.12.14",
+    version="0.20.0",
     packages=["small_asc"],
     package_data={
         "small-asc": ["py.typed"],
