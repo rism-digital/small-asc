5ed4e80d7350f5b01d03c9291480ee4888020a85
Andrew Hankinson <andrew.hankinson@gmail.com>
2024-10-24 10:48:54 +0200

    Allow punctuation in literals

--- tests/test_query.py
+++ tests/test_query.py
@@ -24,6 +24,7 @@ test_queries = [
     ("[* TO 20]", "[* TO 20]"),
     ("Blæ", "Blæ"),
     ('creator:Beethoven AND "sonata C"~4', 'creator:Beethoven AND "sonata C"~4'),
+    ('publisher:"G.H."', 'publisher:"G.H."'),
 ]
 
 
