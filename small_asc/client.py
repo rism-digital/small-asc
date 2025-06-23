8c3eb370a63d48a99528cc029087da6113860b0c
Andrew Hankinson <andrew.hankinson@gmail.com>
2025-06-23 15:35:51 +0200

    Fixed: Iteration would stop after first result on second page

--- small_asc/client.py
+++ small_asc/client.py
@@ -205,7 +205,8 @@ class Results:
 
                     if self.docs:
                         yield self.docs[self._page_idx]
-                    break
+                    else:
+                        break
 
                 self._page_idx += 1
                 self._idx += 1
